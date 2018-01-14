4ab9bf5d81b377d194571fc55b79abc84a150931
Erno Wever <ernowever@Ernos-MacBook-Pro.local>
2018-01-14 17:03:42 +0100

    Add vote button to PostSnippet

--- src/components/PostSnippet.js
+++ src/components/PostSnippet.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 import { PostType } from '../utils/PropTypes'
+import { addVote } from '../actions'
+import Vote from './Vote'
 
 class PostSnippet extends Component {
   static propTypes = {
     post: PostType.isRequired,
+    addVote: PropTypes.func.isRequired,
+  }
+
+  castVote = () => {
+    const vote = 'upVote' //TODO: swithc between upvote and downvote (and remember state)
+    this.props.addVote(this.props.post.id, vote)
   }
 
   render() {
@@ -26,9 +36,14 @@ class PostSnippet extends Component {
         <p>{body}</p>
         Author: {author} | Comments: {commentCount} | Votes: {voteScore} | Date:{' '}
         {timestamp}
+        <Vote onClick={this.castVote} />
       </div>
     )
   }
 }
 
-export default PostSnippet
+const mapDispatchToProps = (dispatch) => ({
+  addVote: (id, vote) => dispatch(addVote(id, vote)),
+})
+
+export default connect(null, mapDispatchToProps)(PostSnippet)
